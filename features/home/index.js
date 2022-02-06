a10e5c64f94048428823142d63580bdc5674c7f9
brhanegiday <brhane5giday@gmail.com>
2022-02-06 20:24:21 +0300

    Home page phase-1

--- features/home/index.js
+++ features/home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NextLink from "next/link";
 import Filter from "../../components/FilterFC";
-import Company from "../../components/Company";
+import Company from "./Company";
 // Icons
 import { SearchIcon } from "@heroicons/react/solid";
 // data
