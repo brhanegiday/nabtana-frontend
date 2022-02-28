445aad5ae327ba5e1307c194da24450898759d83
brhanegiday <brhane5giday@gmail.com>
2022-02-28 10:54:43 +0300

    Update companies data seed

--- data/companies.js
+++ data/companies.js
@@ -4,7 +4,7 @@ export const companies = [
   {
     id: uuidv4(),
     companyName: "Gezana",
-    location: "Sans Francisco,California",
+    location: "Sans Francisco,CA",
     companyExcerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     companyDesc:
       "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate",
@@ -20,29 +20,27 @@ export const companies = [
       {
         name: "John Smith",
         position: "CTO",
+        image: "/founders/founder-1.png",
         aboutFounder:
           "John Smith is the co-founder, Chief Strategy Officer, and Chairman of Airbnb China. Nathan plays a leading role in driving key strategic initiatives across the global business. Previously he oversaw the creation of Airbnb’s engineering, data science,",
-        founderSocialLinks: [
-          {
-            facebook: "https://facebook.com/John.smith03",
-            instagram: "https://instagram.com/John.smith03",
-            twitter: "https://twitter.com/John.smith03",
-            linkedin: "https://linkedin.com/John.smith03",
-          },
-        ],
+        founderSocialLinks: {
+          facebook: "https://facebook.com/John.smith03",
+          instagram: "https://instagram.com/John.smith03",
+          twitter: "https://twitter.com/John.smith03",
+          linkedin: "https://linkedin.com/John.smith03",
+        },
       },
       {
         name: "Brian Chesky",
         position: "CEO",
+        image: "/founders/founder-2.png",
         aboutFounder:
           "John Smith is the co-founder, Chief Strategy Officer, and Chairman of Airbnb China. Nathan plays a leading role in driving key strategic initiatives across the global business. Previously he oversaw the creation of Airbnb’s engineering, data science,",
-        founderSocialLinks: [
-          {
-            instagram: "https://instagram.com/brian.smith03",
-            twitter: "https://twitter.com/brian.smith03",
-            linkedin: "https://linkedin.com/ckesky.smith03",
-          },
-        ],
+        founderSocialLinks: {
+          instagram: "https://instagram.com/brian.smith03",
+          twitter: "https://twitter.com/brian.smith03",
+          linkedin: "https://linkedin.com/ckesky.smith03",
+        },
       },
     ],
     companySocialLinks: {
@@ -71,23 +69,21 @@ export const companies = [
     founders: [
       {
         name: "Brhane Giday",
-        image: "",
+        image: "/founders/founder-1.png",
         position: "CTO",
         aboutFounder:
           "Brhane Giday is the co-founder, Chief Strategy Officer, and Chairman of Airbnb China. Nathan plays a leading role in driving key strategic initiatives across the global business. Previously he oversaw the creation of Airbnb’s engineering, data science,",
-        founderSocialLinks: [
-          {
-            facebook: "https://facebook.com/John.smith03",
-            instagram: "https://instagram.com/John.smith03",
-            twitter: "https://twitter.com/John.smith03",
-            linkedin: "https://linkedin.com/John.smith03",
-          },
-        ],
+        founderSocialLinks: {
+          facebook: "https://facebook.com/John.smith03",
+          instagram: "https://instagram.com/John.smith03",
+          twitter: "https://twitter.com/John.smith03",
+          linkedin: "https://linkedin.com/John.smith03",
+        },
       },
       {
-        name: "Mewael Gesesse",
+        name: "Mewael Gese",
         position: "CEO",
-        image: "",
+        image: "/founders/founder-2.png",
         aboutFounder:
           "John Smith is the co-founder, Chief Strategy Officer, and Chairman of Airbnb China. Nathan plays a leading role in driving key strategic initiatives across the global business. Previously he oversaw the creation of Airbnb’s engineering, data science,",
         founderSocialLinks: [
